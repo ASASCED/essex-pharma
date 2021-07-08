3b31912928016e3831ab7386e0689187505a2519
Jesus Jimenez Cordero <jesus.jimenez.cordero.contractor@bbva.com>
2021-07-08 10:08:11 -0500

    Add: Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Countdown from "react-countdown";
 import "./App.scss";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="logo" />
       <h1>ESSEX PHARMA S.A.S. DE C.V.</h1>
       <h2>Espera por nuestra apertura. Muy pronto.</h2>
       <Countdown date={new Date(2021, 7, 12)} />
