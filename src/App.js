ea88be01d74ea0c951e2a93b971521464d54a0ed
Jesus Jimenez Cordero <jesus.jimenez.cordero.contractor@bbva.com>
2021-07-08 10:18:46 -0500

    Fix: Logo

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
   return (
     <div className="App">
       <img src={logo} alt="logo" />
-      <h1>ESSEX PHARMA S.A.S. DE C.V.</h1>
-      <h2>Espera por nuestra apertura. Muy pronto.</h2>
       <Countdown date={new Date(2021, 7, 12)} />
     </div>
   );
